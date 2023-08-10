f7d30da53f519d1f0d7930f022998ec7c1df4c98
Kamel Alrashedy <kamel773@outlook.com>
2023-08-09 21:08:55 -0400

    3

--- Interact_with_Bandit.py
+++
@@ -1,9 +0,0 @@
-
-def row_exists(conn, table, id):
-    cursor = conn.cursor()
-    cursor.execute('SELECT 1 FROM {} WHERE id = %s', (id,))
-    row = cursor.fetchone()
-    if row:
-        return True
-    else:
-        return False
--- self_refine_with_prompt.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-
-def insert_null(db_name, table_name, column_name):
-    conn = sqlite3.connect(db_name)
-    cursor = conn.cursor()
-    cursor.execute("INSERT INTO {} ({}) VALUES (?)".format(table_name, column_name), (None,))
-    conn.commit()
-    conn.close()
